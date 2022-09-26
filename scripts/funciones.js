7f0c4cee62a2e5b45f582d23006d0988c2236213
AngelKamy <ivanvaq3@gmail.com>
2022-09-26 16:13:17 -0500

    update:ultima actualización

---
+++ scripts/funciones.js
@@ -0,0 +1,14 @@
+console.log("Sesión JS02 funciones")
+/**
+ * Este es un ejemplo de función declarada
+ * Realiza la multiplicación de dos numeros
+ * @param {Number} a factor 2
+ * @param {Number} b factor 1 
+ * @returns resultado multiplicación
+ */
+
+function multiplica(a,b){
+    return a*b;
+}
+
+console.log("Multiplica 5 * 6 =" + multiplica(5,6));
