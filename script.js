f3293aa5a8d2268f07bd2a84af26c9d704690279
AngelKamy <ivanvaq3@gmail.com>
2022-09-26 11:02:26 -0500

    ci: pasé mi código script a una hoja independiente

---
+++ script.js
@@ -0,0 +1,7 @@
+ //Este pedazo sirve para cambiarle el coloe a Generation
+ console.log("Saoko papi saoko"); 
+  document.getElementById("generation").style.color="gray"
+  function colorTitulo(){
+    console.log("Pulsó el botón de generation")
+    document.getElementById("generation").style.color="blue"; 
+  }
